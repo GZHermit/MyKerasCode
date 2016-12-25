fa646f2c51ba71646740ddb07c28352f20ecff01
GZHermit <gzhao0726@163.com>
2016-12-25 21:56:31 +0800

    nothing

--- temporary.py
+++ temporary.py
@@ -1,6 +1,27 @@
 # coding : utf-8
+from keras.models import Sequential
+from keras.layers import Dense, Merge
+from keras.optimizers import SGD
 
-from keras.layers import Input, Dense
+left = Sequential()
+left.add(Dense(output_dim=32, input_dim=64))
 
-x = Input(shape=(100, 50), name='x')
-y = Dense()(x)
+right = Sequential()
+right.add(Dense(output_dim=16, input_dim=128))
+
+merged = Merge(layers=[left, right], mode='concat')
+
+end = Sequential()
+end.add(merged)
+end.add(Dense(5, activation='softmax'))
+
+sgd = SGD(lr=0.01, momentum=0.9, decay=1e-6, nesterov=True, )
+end.compile(optimizer=sgd, loss="categorical_crossentropy", metrics=['accuracy'])
+
+import numpy as np
+
+leftdata = np.random.random()
+rightdata = np.random.random()
+labels = np.random.randint()
+
+end.fit(x=[leftdata, rightdata], y=labels)
--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 # coding: utf-8
-
-from keras.layers import merge, Dense, LSTM, Input, Embedding, Merge
+from keras.layers import merge, Dense, LSTM, Input, Embedding
 from keras.models import Model
 
 # 超參數
@@ -8,23 +7,39 @@ nb_dataset = 1010  # 序列样本总数
 nb_simple_seq = 100  # 单个序列的单词数量
 time_step = 20  # 样本时间片数量
 dict_kinds = 1e4  # 字典种类
-news_len = 128
+news_len = 128  # 额外输入信息长度
+
 # 建模型
 main_input = Input(shape=(nb_simple_seq,), name='main_input')
 # Embedding的作用是将字符/单词转换成向量，作用类似word2vec
 x = Embedding(output_dim=512, input_dim=dict_kinds, input_length=nb_simple_seq)(main_input)
 lstm_out = LSTM(32)(x)
-aux_output = Dense(1, activation='sigmod', name='aux_output')(lstm_out)
+aux_output = Dense(1, activation='sigmoid', name='aux_output')(lstm_out)
 aux_input = Input(shape=(news_len,), name='aux_input')
 x = merge(inputs=[lstm_out, aux_input], mode='concat')
-
 x = Dense(64, activation='relu')(x)
 x = Dense(64, activation='relu')(x)
 x = Dense(64, activation='relu')(x)
+main_output = Dense(1, activation='sigmoid', name='main_output')(x)
 
-main_output = Dense(1, activation='sigmod', name='main_output')(x)
+# 定义模型Tensor -> model
+model = Model(input=[main_input, aux_input], output=[main_output, aux_output])
 
-
+# 生成输入数据
 import numpy as np
 
-# data = np.random.random((nb_dataset, time_step, data_dim))
+headline_data = np.random.random(size=(nb_dataset,time_step,))
+additional_data = np.random.random()
+labels = np.random.randint()
+
+# 编译模型
+# model.compile(optimizer='rmsprop', loss='binary_crossentropy', loss_weights=[1., 0.2])
+model.compile(optimizer='rmsprop',
+              loss={'main_output': 'binary_crossentropy', 'aux_output': 'binary_crossentropy'},
+              loss_weights={'main_output': 1., 'aux_output': 0.2})
+
+# 传输数据运行模型
+# model.fit(x=[headline_data, additional_data], y=[labels, labels], nb_epoch=20, batch_size=32)
+model.fit({'main_input': headline_data, 'aux_input': aux_input},
+          {'main_output': labels, 'aux_output': labels},
+          nb_epoch=20, batch_size=32)
